6927a083bec371b8f5c7af03869b59a475002e27
jr4fs <jr4fs@virginia.edu>
2020-05-31 19:55:58 -0400

    changed options

--- options.py
+++ options.py
@@ -36,15 +36,15 @@ class TemporalModelOptions:
                                  help="number of resnet layers",
                                  default=18,
                                  choices=[18, 34, 50])
-        self.parser.add_argument("--disparity_smoothness",
-                                 type=float,
-                                 help="disparity smoothness weight",
-                                 default=1e-3)
-        self.parser.add_argument("--scales",
-                                 nargs="+",
-                                 type=int,
-                                 help="scales used in the loss",
-                                 default=[0, 1, 2])
+        #self.parser.add_argument("--disparity_smoothness",
+        #                         type=float,
+        #                         help="disparity smoothness weight",
+        #                         default=1e-3)
+        #self.parser.add_argument("--scales",
+        #                         nargs="+",
+        #                         type=int,
+        #                         help="scales used in the loss",
+        #                         default=[0, 1, 2])
         self.parser.add_argument("--min_depth",
                                  type=float,
                                  help="minimum depth",
@@ -84,11 +84,12 @@ class TemporalModelOptions:
                                  help="which training split to use",
                                  choices=["eigen_zhou", "eigen_full", "odom", "benchmark", "apollo"],
                                  default="eigen_zhou")
-        self.parser.add_argument("--num_layers",
-                                 type=int,
-                                 help="number of resnet layers",
-                                 default=18,
-                                 choices=[18, 34, 50, 101, 152])
+        
+        #self.parser.add_argument("--num_layers",
+        #                         type=int,
+        #                         help="number of resnet layers",
+        #                         default=18,
+        #                         choices=[18, 34, 50, 101, 152])
         self.parser.add_argument("--dataset",
                                  type=str,
                                  help="dataset to train on",
@@ -114,6 +115,7 @@ class TemporalModelOptions:
                                  type=int,
                                  help="scales used in the loss",
                                  default=[0, 1, 2])
+        '''
         self.parser.add_argument("--min_depth",
                                  type=float,
                                  help="minimum depth",
@@ -122,19 +124,23 @@ class TemporalModelOptions:
                                  type=float,
                                  help="maximum depth",
                                  default=100.0)
+        '''
         self.parser.add_argument("--use_stereo",
                                  help="if set, uses stereo pair for training",
                                  action="store_true")
+        '''
         self.parser.add_argument("--frame_ids",
                                  nargs="+",
                                  type=int,
                                  help="frames to load",
                                  default=[0, -1, 1])
+        '''
         self.parser.add_argument("--own_resnet",
                                  help="if set, use own resnet encoder/decoder",
                                  action="store_true")
 
         # OPTIMIZATION options
+        '''
         self.parser.add_argument("--batch_size",
                                  type=int,
                                  help="batch size",
@@ -143,6 +149,7 @@ class TemporalModelOptions:
                                  type=float,
                                  help="learning rate",
                                  default=1e-4)
+        '''
         self.parser.add_argument("--num_epochs",
                                  type=int,
                                  help="number of epochs",
@@ -165,10 +172,10 @@ class TemporalModelOptions:
         self.parser.add_argument("--predictive_mask",
                                  help="if set, uses a predictive masking scheme as in Zhou et al",
                                  action="store_true")
-        self.parser.add_argument("--no_ssim",
-                                 help="if set, disables ssim in the loss",
-                                 action="store_true")
-        self.parser.set_defaults(no_ssim=False)
+        #self.parser.add_argument("--no_ssim",
+        #                         help="if set, disables ssim in the loss",
+        #                         action="store_true")
+        #self.parser.set_defaults(no_ssim=False)
         self.parser.add_argument("--weights_init",
                                  type=str,
                                  help="pretrained or scratch",
